5ef7a39ed7ed11480fd5ae96fdc7000fc8634b8e
Galina-Musatova <140553031+Galina-Musatova@users.noreply.github.com>
2025-02-16 22:42:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+import { renderPictures } from './picture.js';
+import { getData, sendData } from './api.js';
+import { showAlert } from './util.js';
+import { setOnFormSubmit, hideModal } from './form.js';
+import { showSuccessMessage, showErrorMessage } from './message.js';
+
+const onSendDataSuccess = () => {
+  hideModal();
+  showSuccessMessage();
+};
+
+const onSendDataError = () => {
+  showErrorMessage();
+};
+
+setOnFormSubmit(async (data) => {
+  await sendData(onSendDataSuccess, onSendDataError, data);
+});
+
+getData(renderPictures, showAlert);
